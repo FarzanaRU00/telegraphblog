5e069fdb7ea4fcc20e59f5250847934746d0d087
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-04-06 19:32:21 +0100

    sql db

---
+++ db/2_insertinto.sql
@@ -0,0 +1,25 @@
+INSERT INTO blogstable (title, author, content, year, month, day)
+VALUES(
+    'Hello hi',
+    'namedoesnotexist',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    2022,
+    04,
+    06
+),
+(
+    'Practise two',
+    'falsename',
+    'Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet',
+    2022,
+    04,
+    06
+),
+(
+    'Practise three',
+    'notreal',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    2022,
+    04,
+    06
+);
---
+++ dbConfig.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+
+const pool = new Pool ({
+    database: process.env.PGDATABASE,
+    user: "postgres",
+    password: "*************",
+    database: "anonblog",
+    host: "localhost",
+    port: 5432
+});
+
+module.exports = pool
