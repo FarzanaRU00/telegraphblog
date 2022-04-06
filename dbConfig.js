41bf9efb9b2d3cb45d0585d0579575de3e134bec
FarzanaRU00 <Farzana29@hotmail.co.uk>
2022-04-06 19:42:41 +0100

    docker-compose.yaml edits

--- dbConfig.js
+++ dbConfig.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 const pool = new Pool ({
     database: process.env.PGDATABASE,
     user: "postgres",
-    password: "*************",
-    database: "anonblog",
+    password: "****************",
+    database: "anonentriesdb",
     host: "localhost",
     port: 5432
 });
